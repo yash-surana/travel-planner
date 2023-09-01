72088154267aca58c333cb7fa502232399daf4a9
yash-surana <75572841+yash-surana@users.noreply.github.com>
2023-09-01 18:34:52 +0530

    Fixed breaking images
    Signed-off-by: yash-surana <75572841+yash-surana@users.noreply.github.com>

--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="w-full h-[87px] bg-primaryBlue pt-11 pb-4 px-8 lg:px-12 flex flex-row justify-between items-center text-white">
       <a href="/trips">
         <img
-          src="/src/images/logo/wander-us-logo.svg"
+          src="/images/logo/wander-us-logo.svg"
           alt="Wander Us Logo"
           width={102}
           height={40}
@@ -11,7 +11,7 @@ const Navbar = () => {
         />
       </a>
       <img
-        src="/src/images/menu.svg"
+        src="/images/menu.svg"
         alt="Click to view menu"
         width={24}
         height={24}
